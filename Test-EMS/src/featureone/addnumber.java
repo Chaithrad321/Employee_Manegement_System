0ee78c04f62050b8fe455fd6ac00e3b1d277b06e
Chaithrad321 <chaithrad123@gmail.com>
2019-01-27 15:50:37 +0530

    Adding intial project structure

---
+++ Test-EMS/src/featureone/addnumber.java
@@ -0,0 +1,14 @@
+package featureone;
+
+public class addnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=10;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
